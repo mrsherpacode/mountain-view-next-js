238f835dd6da426257421bbc7b454b604839562b
mrsherpacode <sherpacoding@gmail.com>
2026-03-24 20:47:18 -0500

    useTransiton hook is used to show loading spinner

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,16 +1,37 @@
 "use client";
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { deleteReservation } from "@/app/_lib/actions";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
-  return (
+  // useTransition is a React hook for marking an update as non-urgent so the UI stays responsive while work happens.
+  const [isPending, startTransition] = useTransition();
+
+  // delete reservation
+  function handleDelete() {
+    if (!confirm("Are you sure you want to delete reservation")) return;
     //deleteReservation is a Server Action because it is exported from a file with "use server" in actions.js
+    // startTransition(fn): wraps state/action work that can run in the background.
+    startTransition(() => deleteReservation({ bookingId }));
+  }
+  return (
     <button
-      onClick={() => deleteReservation({ bookingId })}
+      onClick={handleDelete}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      <>
+        {!isPending ? (
+          <>
+            <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+            <span className="mt-1">Delete</span>
+          </>
+        ) : (
+          <span className="mx-auto">
+            <SpinnerMini />
+          </span>
+        )}
+      </>
     </button>
   );
 }

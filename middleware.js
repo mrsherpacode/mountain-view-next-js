2436b0a39b668a2e4ac9874417b236dbf36bf44f
mrsherpacode <sherpacoding@gmail.com>
2026-03-29 20:27:27 -0500

    final commit:

--- middleware.js
+++
@@ -1,8 +0,0 @@
-//Think of middleware like a security guard at a VIP room.
-// does authorization for /account routes.
-import { auth } from "@/app/_lib/auth";
-export const middleware = auth;
-// Here name should be config and it only protects accout route
-export const config = {
-  matcher: ["/account"],
-};

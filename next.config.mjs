cb94814d9d0c2f8dffc8b7ad608566b46e020edc
mrsherpacode <sherpacoding@gmail.com>
2026-03-29 20:39:15 -0500

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const projectRoot = fileURLToPath(new URL("./", import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: projectRoot,
   turbopack: {
     root: projectRoot,
   },
